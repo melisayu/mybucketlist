a300e5cd3afa0b58f525da34005922cb494d1f4a
melisayu <melisayuliakristin@gmail.com>
2021-02-19 09:45:05 +0100

    Add Whitenoise to WSGI

--- mybucketlist/wsgi.py
+++ mybucketlist/wsgi.py
@@ -11,7 +11,9 @@ import os
 import sys
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mybucketlist.settings')
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
