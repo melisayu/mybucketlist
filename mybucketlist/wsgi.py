4bb15bea8c1697cc03f1d37766950057d2ceca8f
melisayu <melisayuliakristin@gmail.com>
2021-02-19 09:49:38 +0100

    Remove Whitenoise Config from wsgi

--- mybucketlist/wsgi.py
+++ mybucketlist/wsgi.py
@@ -11,9 +11,7 @@ import os
 import sys
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mybucketlist.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
